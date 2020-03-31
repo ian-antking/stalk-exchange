843514acfcfbd52f73380bf2d9ad4744bffb47fd
Ian King <ian.antking@gmail.com>
2020-03-31 13:29:50 +0100

    switch to using scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/App.css';
+import './styles/App.scss';
 
 class App extends React.Component{
   render() {
---
+++ src/components/nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from 'react-bulma-components/lib/components/navbar';
+
+const Nav = () => (
+  <Navbar>
+
+  </Navbar>
+);
+
+export default Nav;
