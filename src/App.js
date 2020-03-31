c44ae8005a58d4d6abdb877c08c6555add3e7c1c
Ian King <ian.antking@gmail.com>
2020-03-31 13:45:57 +0100

    add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'emotion-theming'
+import theme from '@rebass/preset'
 import Nav from './components/nav';
 
 import './styles/App.scss';
 
 class App extends React.Component{
   render = () => (
+    <ThemeProvider theme={theme}>
       <div className='App'>
         <Nav />
         <h1>Stalk Market</h1>
         <h2>coming soon!</h2>
       </div>
+    </ThemeProvider>
     )
 }
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  Flex,
+  Text,
+  Box,
+  Link,
+} from 'rebass';
 
 const Nav = () => (
-  <div>
-    <h1>This will be the navbar</h1>
-  </div>
+  <Flex
+  px={2}
+  color='white'
+  bg='black'
+  alignItems='center'>
+  <Text p={2} fontWeight='bold'>Stalk Market</Text>
+  <Box mx='auto' />
+  <Link variant='nav' href='/signup'>
+    sign-up
+  </Link>
+  <Link variant='nav' href='login'>
+    log-in
+  </Link>
+</Flex>
 );
 
 export default Nav;
