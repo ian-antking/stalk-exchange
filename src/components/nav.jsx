f8f65f8dc6101a1685ee00288d6bc887883e632e
Ian King <ian.antking@gmail.com>
2020-03-31 16:33:15 +0100

    signup and login links only show when not logged in

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,21 +6,27 @@ import {
   Link,
 } from 'rebass';
 
-const Nav = () => (
-  <Flex
-  px={2}
-  color='white'
-  bg='black'
-  alignItems='center'>
-  <Text p={2} fontWeight='bold'>Stalk Market</Text>
-  <Box mx='auto' />
-  <Link variant='nav' href='/signup'>
-    sign-up
-  </Link>
-  <Link variant='nav' href='login'>
-    log-in
-  </Link>
-</Flex>
-);
+const Nav = (props) => {
+  return (
+    <Flex
+    px={2}
+    color='white'
+    bg='black'
+    alignItems='center'>
+    <Text p={2} fontWeight='bold'>Stalk Market</Text>
+    <Box mx='auto' />
+    { !props.isLoggedIn && (
+      <React.Fragment>
+        <Link variant='nav' href='/signup'>
+          sign-up
+        </Link>
+        <Link variant='nav' href='login'>
+          log-in
+        </Link>
+      </React.Fragment>
+    ) }
+  </Flex>
+  );
+}
 
 export default Nav;
