5930128d430b8fa4273066fe5a5d2d4c2af8cdaa
Ian King <ian.antking@gmail.com>
2020-03-31 10:47:58 +0100

    remove default app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './assets/logo.svg';
 import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> to build a gui.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    return (
+      <div className='App'>
+        <h1>Stalk Market</h1>
+      </div>
+    )
+  };
 }
 
 export default App;
