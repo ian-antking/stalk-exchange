bb19902af645925adcd3ac22ce268059f1e058db
Ian King <ian.antking@gmail.com>
2020-03-31 14:35:13 +0100

    add token manager

---
+++ src/utils/token-manager.js
@@ -0,0 +1,29 @@
+import decode from 'jwt-decode';
+
+const setToken = (token) => {
+  window.localStorage.setItem('apiToken', token);
+};
+
+const getToken = () => window.localStorage.getItem('apiToken');
+
+const getTokenPayload = () => {
+  const token = getToken();
+  return token && decode(token);
+};
+
+const isTokenValid = () => {
+  const token = getTokenPayload();
+  return Boolean(token && ((!token.exp) || (Date.now() < token.exp)));
+};
+
+const removeToken = () => {
+  window.localStorage.removeItem('apiToken');
+};
+
+export default {
+  setToken,
+  getToken,
+  getTokenPayload,
+  isTokenValid,
+  removeToken,
+};
