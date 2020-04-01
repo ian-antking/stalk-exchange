772bb9d408ea3373b092f6ba25282450e25924f1
Ian King <ian.antking@gmail.com>
2020-04-01 14:43:25 +0100

    add heading to login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -6,6 +6,7 @@ import {
 import {
   Box,
   Button,
+  Heading,
 } from 'rebass';
 import apiString from '../utils/api-string';
 import TokenManager from '../utils/token-manager';
@@ -47,52 +48,53 @@ class Login extends React.Component {
         this.props.onLogin()
         this.props.history.push('/');
       });
-
   };
 
   render() {
     return (
-      <Box
-        as='form'
-        submit={event => this.preventDefault(event)}
-        py={3}
-        width={2/3}
-        margin='auto'
-      >
-      <Label py={2} htmlFor='login_name_input'>Name</Label>
-      <Input
-        id='login_name_input'
-        name='name'
-        required
-        value={this.state.fields.name}
-        onChange={event => this.handleFieldChange(event)}
-      />
-      <Label py={2} htmlFor='login_island_input'>Island</Label>
-      <Input
-        id='login_island_input'
-        name='island'
-        required
-        value={this.state.fields.island}
-        onChange={event => this.handleFieldChange(event)}
-      />
-      <Label py={2} htmlFor='login_password_input'>Password</Label>
-      <Input
-        id='login_password_input'
-        name='password'
-        required
-        type='password'
-        value={this.state.fields.password}
-        onChange={event => this.handleFieldChange(event)}
-      />
-      <Button
-        my={3}
-        width='100%'
-        varient='primary'
-        onClick={this.handleLogin}
-      >
-        Login
-      </Button>
-      </Box>
+      <React.Fragment>
+        <Heading my={3}>Login</Heading>
+        <Box
+          as='form'
+          submit={event => this.preventDefault(event)}
+          width={2/3}
+          margin='auto'
+        >
+        <Label py={2} htmlFor='login_name_input'>Name</Label>
+        <Input
+          id='login_name_input'
+          name='name'
+          required
+          value={this.state.fields.name}
+          onChange={event => this.handleFieldChange(event)}
+        />
+        <Label py={2} htmlFor='login_island_input'>Island</Label>
+        <Input
+          id='login_island_input'
+          name='island'
+          required
+          value={this.state.fields.island}
+          onChange={event => this.handleFieldChange(event)}
+        />
+        <Label py={2} htmlFor='login_password_input'>Password</Label>
+        <Input
+          id='login_password_input'
+          name='password'
+          required
+          type='password'
+          value={this.state.fields.password}
+          onChange={event => this.handleFieldChange(event)}
+        />
+        <Button
+          my={3}
+          width='100%'
+          varient='primary'
+          onClick={this.handleLogin}
+        >
+          Login
+        </Button>
+        </Box>
+      </React.Fragment>
     );
   }
 }
