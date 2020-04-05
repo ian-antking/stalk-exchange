e13450bd921a06ee785add18ce70a419337251e5
Ian King <ian.antking@gmail.com>
2020-04-05 11:42:44 +0100

    add current period helper

--- src/utils/date-helpers.js
+++ src/utils/date-helpers.js
@@ -4,6 +4,10 @@ const sameDay = (date1, date2) => {
   return format(date1, 'dLy') === format(date2, 'dLy');
 }
 
+const currentPeriod = () => {
+  return format(Date.now(), 'a..aaa');
+}
+
 const samePeriod = (date1, date2) => {
   return format(date1, 'a..aaa') === format(date2, 'a..aaa');
 }
