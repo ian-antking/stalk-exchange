6632b81fb84b2b23dd3f40a00b62b17af6c821bc
Ian King <ian.antking@gmail.com>
2020-04-12 15:10:22 +0100

    helper method to generate friend codes

--- server/__tests__/helpers/data-factory.js
+++ server/__tests__/helpers/data-factory.js
@@ -1,9 +1,26 @@
 const faker = require('faker');
 
+const randomDigit = () => {
+  const digits = '123456890'.split('');
+  return digits[Math.floor(Math.random() * digits.length)];
+}
+
+const randomFourDigits = () => {
+  const fourDigits = [];
+  for (let i = 0; i < 4; i += 1) {
+    fourDigits.push(randomDigit())
+  }
+  return fourDigits.join('');
+}
+
+const generateFriendCode = () => {
+  return `SW-${randomFourDigits()}-${randomFourDigits()}-${randomFourDigits()}`
+}
+
 exports.user = (options = {}) => ({
   name: options.name || faker.name.findName(),
   island: options.name || faker.name.firstName(),
   password: options.password || faker.internet.password(),
-  friendCode: options.friendCode || faker.internet.password(),
+  friendCode: options.friendCode || generateFriendCode(),
   inviteCode: options.inviteCode || 'invite',
 });
