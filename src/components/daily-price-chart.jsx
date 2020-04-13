6715ce84ea1ee6368f2e7c03d1ab70c833c1d5ab
Ian King <ian.antking@gmail.com>
2020-04-13 10:33:19 +0100

    adjust graph y axis range

--- src/components/daily-price-chart.jsx
+++ src/components/daily-price-chart.jsx
@@ -9,42 +9,54 @@ import {
 } from '../utils/filter-helpers';
 import { Heading } from 'rebass';
 
-const DailyPriceChart = props => {
+const DailyPriceChart = (props) => {
   const data = [['Period', 'Bells']];
   const userPrices = filterUserPrices(props.prices, props.user);
   const thisWeeksPrices = filterThisWeeksPrices(userPrices);
   const thisWeeksSellPrices = filterThisWeeksSellPrices(thisWeeksPrices);
   const thisWeeksPurchasePrice = findThisWeeksPurchasePrice(thisWeeksPrices);
-  thisWeeksSellPrices.forEach(price => {
+  thisWeeksSellPrices.forEach((price) => {
     const date = format(price.date, 'E a');
     data.push([date, price.bells]);
   });
 
-  const baseline = thisWeeksPurchasePrice ? thisWeeksPurchasePrice.bells : null
+  const baseline = thisWeeksPurchasePrice ? thisWeeksPurchasePrice.bells : null;
 
   const options = {
-    vAxis: {baseline, baselineColor: 'red' },
+    vAxis: {
+      baseline,
+      baselineColor: 'red',
+      viewWindow: {
+        min: 0,
+        max: 600,
+      },
+    },
     legend: 'none',
     chartArea: {
-      backgroundColor: "transparent",
+      backgroundColor: 'transparent',
       width: '80%',
       height: '80%',
       marginTop: '10%',
-    }
+    },
   };
 
-  return thisWeeksSellPrices.length > 0 && (
-    <React.Fragment>
-      <Heading mt={3}>{`${props.user.name}'s prices for week ${format(Date.now(), 'w')}`}</Heading>
-      <Chart
-        chartType="LineChart"
-        data={data}
-        options={options}
-        width="100%"
-        height="400px"
-        legendToggle
-      />
-    </React.Fragment>
+  return (
+    thisWeeksSellPrices.length > 0 && (
+      <React.Fragment>
+        <Heading mt={3}>{`${props.user.name}'s prices for week ${format(
+          Date.now(),
+          'w'
+        )}`}</Heading>
+        <Chart
+          chartType="LineChart"
+          data={data}
+          options={options}
+          width="100%"
+          height="400px"
+          legendToggle
+        />
+      </React.Fragment>
+    )
   );
 };
 
