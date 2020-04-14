5e63b323cd852d589184c186b9d28399d3d318c5
Ian King <ian.antking@gmail.com>
2020-04-14 16:32:16 +0100

    add bells icon to best price card

--- src/components/best-price-card.jsx
+++ src/components/best-price-card.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Card, Heading, Text } from 'rebass';
+import { Box, Card, Heading, Text, Image } from 'rebass';
 import { isSunday } from 'date-fns';
 import { lowestPrice, highestPrice } from '../utils/filter-helpers';
+import bells from '../images/bells.svg'
 
 const BestPriceCard = props => {
   const bestPrice = isSunday(Date.now()) ? lowestPrice(props.prices) : highestPrice(props.prices);
   return (
     <Card>
       <Heading>Current Best Price:</Heading>
-      <Text>{`${bestPrice.bells} bells`}</Text>
+      <Box
+        display='flex'
+        width='100%'
+        justifyContent='center'
+        my={3}
+      >
+        <Text fontSize={4}>{bestPrice.bells}</Text>
+        <Image mx={1} src={bells} alt='bells'/>
+      </Box>
       <Text>{`On ${bestPrice.user.name}'s Island: ${bestPrice.user.island}`}</Text>
       <Text>{`${bestPrice.user.dodoCode || ''}`}</Text>
     </Card>
