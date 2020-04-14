4fd9a24ebd17fdcb9517905eb9986c77b2d62613
Ian King <ian.antking@gmail.com>
2020-04-14 19:56:49 +0100

    factor out colors into own file

---
+++ src/utils/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  text: '#39383d',
+  primary: '#71b6f7',
+  secondary: '#ee6668',
+  background: '#b1f5fb',
+  confirm: '#ea4d740',
+  error: '#c93335',
+  muted: '#f5f7e9',
+  white: '#fff',
+  gray: '#b7b398',
+  highlight: '#ffbb5f',
+};
+
+export default colors;
