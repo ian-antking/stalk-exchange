78df217303a7bc1add63b945977d9d3e3ca3cf2f
Ian King <ian.antking@gmail.com>
2020-04-16 13:56:42 +0100

    move chart data creation to helper function

--- src/components/daily-price-chart.jsx
+++ src/components/daily-price-chart.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import { Chart } from 'react-google-charts';
 import { format } from 'date-fns';
-import {
-  filterThisWeeksPrices,
-  filterThisWeeksSellPrices,
-  findThisWeeksPurchasePrice,
-  filterUserPrices,
-} from '../utils/filter-helpers';
+import { Chart } from 'react-google-charts';
 import { Heading, Card } from 'rebass';
 import chartOptions from '../utils/chart-options';
+import chartData from '../utils/chart-data';
 
 const DailyPriceChart = (props) => {
-  const data = [['Period', 'Bells']];
-  const userPrices = filterUserPrices(props.prices, props.user);
-  const thisWeeksPrices = filterThisWeeksPrices(userPrices);
-  const thisWeeksSellPrices = filterThisWeeksSellPrices(thisWeeksPrices);
-  const thisWeeksPurchasePrice = findThisWeeksPurchasePrice(thisWeeksPrices);
-  thisWeeksSellPrices.forEach((price) => {
-    const date = format(price.date, 'E a');
-    data.push([date, price.bells]);
-  });
+  const data = chartData(props.prices, props.user)
 
   return (
-    thisWeeksSellPrices.length > 0 && (
+    data.prices.length > 0 && (
       <Card>
         <Heading>{`${props.user.name}'s prices for week ${format(
           Date.now(),
@@ -30,8 +17,8 @@ const DailyPriceChart = (props) => {
         )}`}</Heading>
         <Chart
           chartType="LineChart"
-          data={data}
-          options={chartOptions(thisWeeksPurchasePrice.bells || null)}
+          data={data.prices}
+          options={chartOptions(data.baseline.bells || null)}
           width="100%"
           height="400px"
           legendToggle
