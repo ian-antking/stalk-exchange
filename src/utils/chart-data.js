13362eecb42f52a52c55a9ef425e882b3126d4c2
Ian King <ian.antking@gmail.com>
2020-04-22 14:15:55 +0100

    add price prediction

--- src/utils/chart-data.js
+++ src/utils/chart-data.js
@@ -4,16 +4,27 @@ import {
   findThisWeeksPurchasePrice,
 } from './filter-helpers';
 import { format } from 'date-fns';
+import { possiblePatterns, patternReducer } from './pattern';
 
 const chartData = (user) => {
-  const data = [['Period', 'Bells']];
+  const data = [['Period', 'Bells', 'Min', 'Max']];
   const thisWeeksPrices = filterThisWeeksPrices(user.prices);
   const thisWeeksSellPrices = filterThisWeeksSellPrices(thisWeeksPrices);
   const thisWeeksPurchasePrice = findThisWeeksPurchasePrice(thisWeeksPrices);
-  thisWeeksSellPrices.forEach((price) => {
-    const date = format(price.date, 'E a');
-    data.push([date, price.bells]);
-  });
+  const analysis = [thisWeeksPurchasePrice.bells];
+
+  thisWeeksSellPrices.forEach(price => analysis.push(price.bells))
+
+  const patterns = possiblePatterns(analysis);
+  const minMaxPattern = patternReducer(patterns);
+
+  const dates = [ 'Mon Am', 'Mon PM', 'Tue Am', 'Tue PM', 'Wed Am', 'Wed PM', 'Thu Am', 'Thu PM', 'Fri Am', 'Fri PM', 'Sat Am', 'Sat PM',  ]
+
+  for (let i = 1; i < minMaxPattern.length; i += 1) {
+    const price = thisWeeksSellPrices[i] ? thisWeeksSellPrices[i].bells : null
+    const date = dates[i];
+    data.push([date, price, minMaxPattern[i][0], minMaxPattern[i][1]]);
+  }
 
   return {
     baseline: thisWeeksPurchasePrice,
