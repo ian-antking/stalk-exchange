313dc8903dcf9ac9982106e290869734b97cddef
Ian King <ian.antking@gmail.com>
2020-04-22 09:36:57 +0100

    add loading card

---
+++ src/components/loading-card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Heading, Card } from 'rebass';
+
+const LoadingCard = () => (
+  <Card py={5} alignItems='center'>
+    <Heading>Loading...</Heading>
+  </Card>
+)
+
+export default LoadingCard;
