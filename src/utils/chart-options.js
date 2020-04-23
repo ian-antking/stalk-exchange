f505bf5433e6dc64aa2d1f573d72c5e8b426b682
Ian King <ian.antking@gmail.com>
2020-04-23 09:21:01 +0100

    position chart legend in center

--- src/utils/chart-options.js
+++ src/utils/chart-options.js
@@ -1,21 +1,20 @@
 import colors from './colors';
 
 const chartOptions = (baseline = null) => ({
-    vAxis: {
-      baseline,
-      baselineColor: colors.error,
-    },
-    colors: [colors.primary, colors.secondary, colors.confirm],
-    lineWidth: 3,
-    backgroundColor: 'transparent',
-    legend: { position: 'top' },
-    chartArea: {
-      width: '80%',
-      height: '80%',
-      marginTop: '10%',
-    },
-    curveType: 'function',
-  }
-)
+  vAxis: {
+    baseline,
+    baselineColor: colors.error,
+  },
+  colors: [colors.primary, colors.secondary, colors.confirm],
+  lineWidth: 3,
+  backgroundColor: 'transparent',
+  legend: { position: 'top', alignment: 'center' },
+  chartArea: {
+    width: '80%',
+    height: '80%',
+    marginTop: '10%',
+  },
+  curveType: 'function',
+});
 
 export default chartOptions;
