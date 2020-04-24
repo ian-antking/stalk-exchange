fdd4bdc5d5200536928e70fca725e862cfb2d221
Ian King <ian.antking@gmail.com>
2020-04-24 11:22:16 +0100

    pass app methods to missing-prices

--- src/components/daily-price-chart.jsx
+++ src/components/daily-price-chart.jsx
@@ -10,27 +10,31 @@ import tokenManager from '../utils/token-manager';
 const DailyPriceChart = (props) => {
   const { user } = props;
 
-  const data = user.prices.length && chartData(user)
-  
-  return (
-    data ? (
-      <Card>
-        <Heading>{`${props.user.name}'s prices for week ${format(
-          Date.now(),
-          'w'
-        )}`}</Heading>
-        <Chart
-          chartType="LineChart"
-          data={data.prices}
-          options={chartOptions(data.baseline ? data.baseline.bells : null)}
-          width="100%"
-          height="400px"
-          legendToggle
+  const data = user.prices.length && chartData(user);
+
+  return data ? (
+    <Card>
+      <Heading>{`${props.user.name}'s prices for week ${format(
+        Date.now(),
+        'w'
+      )}`}</Heading>
+      <Chart
+        chartType="LineChart"
+        data={data.prices}
+        options={chartOptions(data.baseline ? data.baseline.bells : null)}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
+      {user._id === tokenManager.getTokenPayload()._id && (
+        <MissingPrices
+          refreshPrices={props.refreshPrices}
+          setMessage={props.setMessage}
+          MissingPrices={data.missing}
         />
-        { user._id === tokenManager.getTokenPayload()._id && <MissingPrices MissingPrices={data.missing} />}
-      </Card>
-    ) : null
-  );
+      )}
+    </Card>
+  ) : null;
 };
 
 export default DailyPriceChart;
